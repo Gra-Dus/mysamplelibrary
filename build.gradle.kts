c3ee490be8d7cd275a2b6d24e07f2a43c0cfc4e8
Gra_Dus <134303030+Skl-RV@users.noreply.github.com>
2023-08-10 17:46:06 +0300

    ref

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
+buildscript {
+    dependencies {
+        classpath("com.github.dcendents:android-maven-gradle-plugin:1.5")
+    }
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.github.dcendents.android-maven") version "1.5" apply false
 }
